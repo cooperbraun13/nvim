4c569d936fc09c80b3980eddee4d9f2bfd0f96b0
cooperbraun13 <cooperjbraun13@gmail.com>
2025-12-27 01:12:41 -0800

    feat: added delve for integrated debugging

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -125,6 +125,40 @@ return {
 		},
 	},
 
+	-- Debugging
+	{
+		"mfussenegger/nvim-dap",
+		dependencies = {
+			"leoluz/nvim-dap-go",
+			"rcarriga/nvim-dap-ui",
+			"nvim-neotest/nvim-nio",
+		},
+		config = function()
+			local dap = require("dap")
+			local dapui = require("dapui")
+
+			require("dap-go").setup()
+			dapui.setup()
+
+			-- Auto open/close UI
+			dap.listeners.after.event_initialized["dapui_config"] = function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated["dapui_config"] = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited["dapui_config"] = function()
+				dapui.close()
+			end
+			-- Keymaps
+			vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint)
+			vim.keymap.set("n", "<leader>dc", dap.continue)
+			vim.keymap.set("n", "<leader>do", dap.step_over)
+			vim.keymap.set("n", "<leader>di", dap.step_into)
+			vim.keymap.set("n", "<leader>du", dapui.toggle)
+		end,
+	},
+
 	-- Better hover/diagnostics UI
 	{
 		"folke/trouble.nvim",
