425e00eb8441035d0f64e6330090d14a8fb745f4
cooperbraun13 <cooperjbraun13@gmail.com>
2025-12-30 00:23:08 -0800

    feat: added refactoring and test plugins for better use of config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -169,4 +169,92 @@ return {
 			{ "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics" },
 		},
 	},
+
+	-- Refactoring
+	{
+		"ThePrimeagen/refactorting.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		opts = {},
+		keys = {
+			{
+				"<leader>tt",
+				function()
+					require("neotest").run.run()
+				end,
+				desc = "Run nearest test",
+			},
+			{
+				"<leader>tf",
+				function()
+					require("neotest").run.run(vim.fn.expand("%"))
+				end,
+				desc = "Run file tests",
+			},
+			{
+				"<leader>ts",
+				function()
+					require("neotest").summary.toggle()
+				end,
+				desc = "Toggle test summary",
+			},
+			{
+				"<leader>to",
+				function()
+					require("neotest").output.open({ enter = true })
+				end,
+				desc = "Show test output",
+			},
+		},
+	},
+
+	-- Testing
+	{
+		"nvim-neotest/neotest",
+		dependencies = {
+			"nvim-neotest/nvim-nio",
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+			"fredrikaverpil/neotest-golang",
+		},
+		config = function()
+			require("neotest").setup({
+				adapters = {
+					require("neotest-golang"),
+				},
+			})
+		end,
+		keys = {
+			{
+				"<leader>tt",
+				function()
+					require("neotest").run.run()
+				end,
+				desc = "Run nearest test",
+			},
+			{
+				"<leader>tf",
+				function()
+					require("neotest").run.run(vim.fn.expand("%"))
+				end,
+				desc = "Run file tests",
+			},
+			{
+				"<leader>ts",
+				function()
+					require("neotest").summary.toggle()
+				end,
+				desc = "Toggle test summary",
+			},
+			{
+				"<leader>to",
+				function()
+					require("neotest").output.open({ enter = true })
+				end,
+				desc = "Show test output",
+			},
+		},
+	},
 }
